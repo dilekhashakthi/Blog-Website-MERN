fcb787aec248734e0938503581878bb32cda2dbc
dilekhashakthi <2000dilekha@gmail.com>
2026-02-17 23:52:34 +0530

    Create the header section

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,18 @@
+import flowbiteReact from "flowbite-react/plugin/tailwindcss";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
+    //Include Flowbite React node_modules
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+    ".flowbite-react\\class-list.json"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbiteReact],
 }
