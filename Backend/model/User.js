ec990ca1009c2764868116b0b1b8a8fb80ba1e24
dilekhashakthi <2000dilekha@gmail.com>
2026-02-19 23:36:31 +0530

    Create user model

---
+++ Backend/model/User.js
@@ -0,0 +1,24 @@
+const mongooes = require("mongooes");
+
+const userSchema = new mongooes.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongooes.model("User", userSchema);
+export default User;
