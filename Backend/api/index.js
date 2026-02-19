f5f19bf1a7c4a4c8903b4c039c9fb3a2da4ed649
dilekhashakthi <2000dilekha@gmail.com>
2026-02-19 23:26:17 +0530

    Connect to the database

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,8 +1,23 @@
-const express = require('express');
+require("dotenv").config({path: '../.env'});
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+async function DBconnection() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database connected successfully...");
+  } catch (error) {
+    console.log("Database connection failed...");
+    console.error(error);
+  }
+}
+
+DBconnection();
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-})
+  console.log(`Server is running on PORT ${PORT}`);
+});
