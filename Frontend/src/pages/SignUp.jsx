c9fcceff4b2e1db3295edc644cf6722ef9d52c07
dilekhashakthi <2000dilekha@gmail.com>
2026-03-01 23:28:40 +0530

    add functionality to the signup page and connect frontend and backend using vite server in vite.config.js file

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,15 +1,58 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value.trim(),
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("All fiels are required..!");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if (response.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-12.5">
-        
         {/* left side */}
         <div className="flex-1">
-          
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-linear-to-r from-slate-900 via-teal-500 to-cyan-400 rounded-lg text-white">
               Nexora
@@ -33,15 +76,15 @@ const SignUp = () => {
 
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
-
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label htmlFor="username">Your username</Label>
               <TextInput
                 type="text"
                 id="username"
+                name="username"
                 placeholder="John"
-                required
+                onChange={handleChange}
               />
             </div>
 
@@ -50,21 +93,35 @@ const SignUp = () => {
               <TextInput
                 type="email"
                 id="email"
+                name="email"
                 placeholder="johndeo@example.com"
-                required
+                onChange={handleChange}
               />
             </div>
 
             <div>
               <Label htmlFor="password">Your password</Label>
-              <TextInput type="password" id="password" required />
+              <TextInput
+                type="password"
+                id="password"
+                name="password"
+                onChange={handleChange}
+              />
             </div>
 
             <Button
               className="bg-linear-to-r from-slate-900 via-teal-500 to-cyan-400"
               type="submit"
+              disabled={loading}
             >
-              Sign Up
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
 
@@ -74,7 +131,12 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
-          
+
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
