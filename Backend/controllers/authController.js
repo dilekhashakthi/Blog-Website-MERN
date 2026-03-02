49442860ced9220a4431c1da8ce12f97ef3bc4b2
dilekhashakthi <2000dilekha@gmail.com>
2026-03-02 10:40:11 +0530

    Create signin api route

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,7 +14,7 @@ const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    return next(errorHandler(400, "All fields are required"))
+    return next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -31,8 +32,43 @@ const signup = async (req, res, next) => {
       message: "Signup successfully!",
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    const { password: pass, ...userWithoutPassword } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const {signup} = require('../controllers/authController');
+const express = require("express");
+const { signup, signin } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post('/signup', signup)
+router.post("/signup", signup);
+router.post("/signin", signin);
 
-module.exports = router
+module.exports = router;
