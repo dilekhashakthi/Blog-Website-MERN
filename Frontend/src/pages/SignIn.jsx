0bc00019627802681ad6268beb61d541dca97e65
dilekhashakthi <2000dilekha@gmail.com>
2026-03-02 11:01:40 +0530

    Complete sign in page functionality

--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default SignIn
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value.trim(),
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("All fiels are required..!");
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      if (response.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-12.5">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-linear-to-r from-slate-900 via-teal-500 to-cyan-400 rounded-lg text-white">
+              Nexora
+            </span>
+            <span className="text-black dark:text-white">Blogs</span>
+          </Link>
+
+          {/* Welcome Text */}
+          <p className="text-sm mt-5">
+            Welcome to Nexora Blogs! <br />
+            Explore, read, and share amazing ideas with the world.
+          </p>
+
+          {/* Mini Description */}
+          <p className="text-sm mt-2">
+            Sign in with your email and password, or continue with Google to
+            start your journey. Discover fresh articles, share your thoughts,
+            and grow your community.
+          </p>
+        </div>
+
+        {/* right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+
+            <div>
+              <Label htmlFor="email">Your email</Label>
+              <TextInput
+                type="email"
+                id="email"
+                name="email"
+                placeholder="johndeo@example.com"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="password">Your password</Label>
+              <TextInput
+                type="password"
+                id="password"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+
+            <Button
+              className="bg-linear-to-r from-slate-900 via-teal-500 to-cyan-400"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account? </span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
