e3be7b90adb14b5a51299c187352d677b363237e
dilekhashakthi <2000dilekha@gmail.com>
2026-04-18 01:23:53 +0530

    show user post inside dashboard

--- Frontend/src/components/DashPosts.jsx
+++ Frontend/src/components/DashPosts.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  Table,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableBody,
+  TableRow,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
 
 const DashPosts = () => {
-  return (
-    <div>DashPosts</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
 
-export default DashPosts
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          `/api/post/getposts?userId=${currentUser._id}`,
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.error("Error fetching user posts:", error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
+
+  return (
+    <div
+      className="table-auto overflow-x-scroll md:mx-auto p-3 
+                scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 
+                dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500"
+    >
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <TableHead>
+              <TableHeadCell>Date Updated</TableHeadCell>
+              <TableHeadCell>Post image</TableHeadCell>
+              <TableHeadCell>Post title</TableHeadCell>
+              <TableHeadCell>Category</TableHeadCell>
+              <TableHeadCell>Delete</TableHeadCell>
+              <TableHeadCell>
+                <span>Edit</span>
+              </TableHeadCell>
+            </TableHead>
+            {userPosts.map((post) => (
+              <TableBody className="divide-y">
+                <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-900">
+                  <TableCell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      to={`/post/${post.slug}`}
+                      className="font-medium text-gray-900 dark:text-white"
+                    >
+                      {post.title}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{post.category}</TableCell>
+                  <TableCell>
+                    <span className="font-medium text-red-500 hover:underline">
+                      Delete
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      to={`/update-post/${post._id}`}
+                      className="text-teal-500 hover:underline"
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+    </div>
+  );
+};
+
+export default DashPosts;
